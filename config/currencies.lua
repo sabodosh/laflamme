f008696aae538f7687c13d1a2f847aff38099111
sabodosh <156852270+sabodosh@users.noreply.github.com>
2024-02-25 21:18:42 +0000

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -12,7 +12,7 @@ end
 
 addCurrency("Деньги",           "customnpcs:npcMoney",      0,   'INGOT', 0x85BB65, nil)
 --addCurrency("Светопыль",        "minecraft:glowstone_dust", 0,   'DUST',  0xD0D000, 5)
---addCurrency("Железный слиток",  "minecraft:iron_ingot",     0,   'INGOT', 0xAAAAAA, nil)
+addCurrency("Железный слиток",  "minecraft:iron_ingot",     0,   'INGOT', 0xAAAAAA, nil)
 --addCurrency("Железный блок",    "minecraft:iron_block",     0,   'BLOCK', 0xAAAAAA, 6)
 --addCurrency("Медный слиток",    "IC2:itemIngot",            0,   'INGOT', 0xA5642F, nil)
 --addCurrency("Медный блок",      "Forestry:resourceStorage", 1,   'BLOCK', 0xA5642F, 6)
