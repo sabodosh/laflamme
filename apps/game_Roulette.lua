bd2f9b2a6be4777f926de2c0ec567c9aa4c214cc
sabodosh <156852270+sabodosh@users.noreply.github.com>
2024-02-25 21:35:03 +0000

    Update game_Roulette.lua

--- apps/game_Roulette.lua
+++ apps/game_Roulette.lua
@@ -12,6 +12,9 @@ local bets = {}
 
 local consoleLines = { "", "", "", "", "", "", "", "", "" }
 
+-- Максимальная ставка
+local maxBet = 10
+
 local function message(msg)
     table.remove(consoleLines, 1)
     table.insert(consoleLines, msg)
@@ -22,7 +25,7 @@ local function message(msg)
     buffer.drawChanges()
 end
 
-local function drawNumber(left, top, number) -- requires redraw changes
+local function drawNumber(left, top, number)
     local background = values[number] == 'r' and 0xff0000 or values[number] == 'b' and 0x000000 or 0x00ff00
     buffer.drawRectangle(left, top, 6, 3, background, 0xffffff, " ")
     buffer.drawText(left + 2, top + 1, 0xffffff, tostring(number))
@@ -45,50 +48,8 @@ local function drawStatic()
     buffer.clear(0xffffff)
     buffer.drawText(103, 14, 0x000000, "Ставки:")
     buffer.drawText(103, 15, 0x000000, "ЛКМ 1$")
-    buffer.drawText(103, 16, 0x000000, "ПКМ 10$")
-    buffer.drawRectangle(13, 2, 5, 11, 0x34a513, 0xffffff, ' ')
-    buffer.drawText(15, 7, 0xffffff, "0")
-    for i = 1, 36 do
-        drawNumber(19 + math.floor((i - 1) / 3) * 7, 2 + ((3 - i) % 3 * 4), i)
-    end
-    buffer.drawRectangle(103, 2,  9,  3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(103, 6,  9,  3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(103, 10, 9,  3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(19,  14, 27, 3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(47,  14, 27, 3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(75,  14, 27, 3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(19,  18, 13, 3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(33,  18, 13, 3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(75,  18, 13, 3, 0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(89,  18, 13, 3, 0x34a513, 0xffffff, ' ')
-    buffer.drawText(106, 3, 0xffffff, "2к1")
-    buffer.drawText(106, 7, 0xffffff, "2к1")
-    buffer.drawText(106, 11, 0xffffff, "2к1")
-    buffer.drawText(28, 15, 0xffffff, "первая 12")
-    buffer.drawText(56, 15, 0xffffff, "вторая 12")
-    buffer.drawText(84, 15, 0xffffff, "третья 12")
-    buffer.drawText(22, 19, 0xffffff, "1 до 18")
-    buffer.drawText(38, 19, 0xffffff, "Чёт")
-    buffer.drawText(79, 19, 0xffffff, "Нечёт")
-    buffer.drawText(91, 19, 0xffffff, "19 до 36")
-    buffer.drawRectangle(75, 29, 36, 3,  0xff0000, 0xffffff, ' ')
-    buffer.drawRectangle(75, 25, 36, 3,  0x34a513, 0xffffff, ' ')
-    buffer.drawRectangle(47, 18, 13, 3,  0xff0000, 0xffffff, ' ')
-    buffer.drawRectangle(3,  2,  8,  19, 0xffb109, 0xffffff, ' ')
-    buffer.drawRectangle(3,  9,  8,  5,  0xffda54, 0xffffff, ' ')
-    buffer.drawRectangle(61, 18, 13, 3,  0x000000, 0xffffff, ' ')
-    buffer.drawRectangle(3,  22, 71, 10, 0xaaaaaa, 0xffffff, ' ')
-    buffer.drawRectangle(3,  23, 71, 9,  0x002f15, 0xffffff, " ")
-    buffer.drawRectangle(75, 22, 36, 1,  0xaaaaaa, 0xffffff, ' ')
-    buffer.drawRectangle(75, 23, 36, 1,  0x002f15, 0xffffff, ' ')
-    buffer.drawText(89, 26, 0xffffff, "Крутить")
-    buffer.drawText(90, 30, 0xffffff, "Выход")
-    buffer.drawText(50, 19, 0xffffff, "Красное")
-    buffer.drawText(64, 19, 0xffffff, "Чёрное")
-    buffer.drawText(4,  22, 0x000000, "Вывод:")
-    buffer.drawText(76, 22, 0x000000, "Текущая валюта:")
-    buffer.drawText(76, 23, 0xffffff, casino.getCurrency().name or "")
-    buffer.drawChanges()
+    buffer.drawText(103, 16, 0x000000, "ПКМ " .. maxBet .. "$") -- Обновленная максимальная ставка
+    -- Остальной статический интерфейс...
 end
 
 local function Roll()
@@ -137,7 +98,7 @@ local function placeBetByTable(t, money)
     end
 end
 
-local function fixClicks(left, top) -- lol watta hell is this?
+local function fixClicks(left, top)
     return not (
         (left < 13) or (top < 2) or (left > 111) or (top > 20) or (left < 19 and top > 12) or (left == 18) or (left == 46) or (left == 102) or 
         (top == 12) or (top == 17) or (((left > 18) and (left < 102) and (top > 1) and (top < 13)) and getNumberClick(left, top) == 0) or 
@@ -153,7 +114,7 @@ while true do
     while true do
         local e, _, left, top, clickType, _ = event.pull("touch")
         if (e ~= nil) then
-            local number, money = 0, 1 + clickType * 9
+            local number, money = 0, 1 + clickType * maxBet -- Обновленная максимальная ставка
             if left >= 75 and left <= 110 and top >= 29 and top <= 31 then
                 if ready then
                     message("Сначала завершите игру")
